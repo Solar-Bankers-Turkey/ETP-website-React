d1369125ad681225a0573a35161b5549dfec6f87
alicanerdurmaz <alicanerdurmaz@gmail.com>
2020-09-07 02:24:17 +0300

    overview

--- components/Charts/PillChart/PillChartItem.tsx
+++ components/Charts/PillChart/PillChartItem.tsx
@@ -10,7 +10,7 @@ const PillChartItem = ({ count, legend }: Props) => {
     <div className={styles.item}>
       <div className={styles.pills}>
         {Array.from({ length: 9 }).map((_, i) => {
-          return <Pill filled={9 - i <= count} />
+          return <Pill key={i} filled={9 - i <= count} />
         })}
       </div>
       <h1 className={styles.legend}>{legend}</h1>
