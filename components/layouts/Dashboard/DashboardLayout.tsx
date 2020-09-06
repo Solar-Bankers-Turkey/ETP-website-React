32a50b773036feba509f375eea02ccfbde6d6070
alicanerdurmaz <alicanerdurmaz@gmail.com>
2020-09-07 02:32:11 +0300

    drawer fix

--- components/layouts/Dashboard/DashboardLayout.tsx
+++ components/layouts/Dashboard/DashboardLayout.tsx
@@ -10,6 +10,20 @@ const DashboardLayout = ({ children }) => {
   const sidebarToggleHandler = () => {
     setSidebarOpen(!isSidebarOpen)
   }
+  useEffect(() => {
+    window.addEventListener('resize', handleResize)
+
+    return () => {
+      window.removeEventListener('resize', handleResize)
+    }
+    function handleResize() {
+      console.log(window.innerWidth)
+      if (window.innerWidth < 1250 && isSidebarOpen) {
+        setSidebarOpen(false)
+      }
+    }
+  }, [])
+
   return (
     <div className={isSidebarOpen ? styles.container_sidebarOpen : styles.container_sidebarClosed}>
       <div className={styles.header}>
