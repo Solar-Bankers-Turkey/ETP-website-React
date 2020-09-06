d2734efdc585369626346e075505fcee30443418
alicanerdurmaz <alicanerdurmaz@gmail.com>
2020-09-06 16:38:37 +0300

    responsive sidebar-header

--- components/layouts/Dashboard/DashboardLayout.tsx
+++ components/layouts/Dashboard/DashboardLayout.tsx
@@ -1,14 +1,32 @@
-import React from 'react'
+import React, { useState, useEffect, useRef } from 'react'
 import styles from './DashboardLayout.module.css'
 import Header from './Header/Header'
 import Sidebar from './Sidebar/Sidebar'
+import cx from 'classnames'
 
 const DashboardLayout = ({ children }) => {
+  const [isSidebarOpen, setSidebarOpen] = useState(true)
+
+  const sidebarToggleHandler = () => {
+    setSidebarOpen(!isSidebarOpen)
+  }
   return (
-    <div className={styles.container}>
-      <Header></Header>
-      <Sidebar></Sidebar>
-      <div style={{ gridArea: 'db-content' }}>{children}</div>
+    <div className={isSidebarOpen ? styles.container_sidebarOpen : styles.container_sidebarClosed}>
+      <div className={styles.header}>
+        <Header sidebarToggleHandler={sidebarToggleHandler}></Header>
+      </div>
+
+      <div className={cx(styles.sidebar)}>
+        <Sidebar></Sidebar>
+      </div>
+
+      <main className={styles.content}>{children}</main>
+
+      <footer className={styles.footer}></footer>
+      <div
+        onClick={() => setSidebarOpen(false)}
+        className={isSidebarOpen ? styles.backdrop : styles.backdrop_off}
+      ></div>
     </div>
   )
 }
--- components/layouts/Dashboard/Header/Header.tsx
+++ components/layouts/Dashboard/Header/Header.tsx
@@ -9,27 +9,31 @@ import MailIcon from '../../../icons/MailIcon'
 import BellIcon from '../../../icons/BellIcon'
 import IconButton from '../../../Button/IconButton'
 
-const Header = () => {
+interface Props {
+  sidebarToggleHandler: () => void
+}
+const Header = ({ sidebarToggleHandler }) => {
   return (
-    <div className={styles.container}>
-      <div className={styles.sidebar_toggle}>
+    <header className={styles.container}>
+      <div className={styles.sidebar_toggle} onClick={sidebarToggleHandler}>
         <BarsIcon />
       </div>
-      <div className={styles.coin_container}>
-        <div className={styles.lock_icon}>
-          <LockIcon></LockIcon>
+      <div className={styles.info_container}>
+        <div className={styles.coin_container}>
+          <div className={styles.lock_icon}>
+            <LockIcon></LockIcon>
+          </div>
+          <span className={styles.coin_amount}>50.000</span>
+          <span className={styles.coin_name}>IYTECOIN</span>
+          <button className={styles.btn_reload}>RELOAD</button>
         </div>
-        <span className={styles.coin_amount}>50.000</span>
-        <span className={styles.coin_name}>IYTECOIN</span>
-        <button className={styles.btn_reload}>RELOAD</button>
-      </div>
-      <div className={styles.energy_container}>
-        <div className={styles.solar_icon}>
-          <SolarPanelIcon />
+        <div className={styles.energy_container}>
+          <div className={styles.solar_icon}>
+            <SolarPanelIcon />
+          </div>
+          <div className={styles.solar_value}>2.6</div>
+          <div className={styles.solar_text}>KWH</div>
         </div>
-        <div className={styles.solar_value}>2.6</div>
-        <div className={styles.solar_text}>KWH</div>
-
         <div className={styles.battery_icon}>
           <BatteryIcon />
         </div>
@@ -43,7 +47,7 @@ const Header = () => {
         <IconButton badge={true} icon={<BellIcon />}></IconButton>
         <div className={styles.btn_username}>Username</div>
       </div>
-    </div>
+    </header>
   )
 }
 
