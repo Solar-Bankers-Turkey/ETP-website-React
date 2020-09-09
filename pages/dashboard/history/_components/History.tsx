d1ed66bf979d66ca0ffc7067b0a7444a037e65ac
alicanerdurmaz <alicanerdurmaz@gmail.com>
2020-09-09 13:04:48 +0300

    history page notifications

--- pages/dashboard/history/_components/History.tsx
+++ pages/dashboard/history/_components/History.tsx
@@ -1,12 +1,15 @@
 import React from 'react'
 import styles from './History.module.css'
 import ChartSection from './ChartSection/ChartSection'
+import NotificationList from '../../../../components/NotificationList/NotificationList'
 
 const History = () => {
   return (
     <div className={styles.container}>
       <h1 className={styles.header}>History</h1>
       <ChartSection />
+
+      <NotificationList />
     </div>
   )
 }
