695e5ed1e58f6fb54868c3436ba6ea76bb2342c0
alicanerdurmaz <alicanerdurmaz@gmail.com>
2020-09-21 15:32:48 +0300

    material ui deleted

--- components/Layouts/Dashboard/Header/Header.tsx
+++ components/Layouts/Dashboard/Header/Header.tsx
@@ -5,7 +5,7 @@ import BarsIcon from '../../../Icons/BarsIcon'
 import MailIcon from '../../../Icons/MailIcon'
 import BellIcon from '../../../Icons/BellIcon'
 import IconButton from '../../../Button/IconButton'
-import HeaderMenu from './HeaderMenu'
+
 import { useLocalizationContext } from '../../../../context/LocalizationContext/LocalizationContext'
 import HeaderInfo from './HeaderInfo'
 import HeaderInfoFloating from './HeaderInfoFloating'
@@ -15,14 +15,6 @@ interface Props {
 }
 const Header = ({ sidebarToggleHandler }) => {
   const { t } = useLocalizationContext()
-  const [anchorEl, setAnchorEl] = useState<null | HTMLElement>(null)
-
-  const handleClick = (event: React.MouseEvent<HTMLDivElement>) => {
-    setAnchorEl(event.currentTarget)
-  }
-  const handleClose = () => {
-    setAnchorEl(null)
-  }
 
   return (
     <header className={styles.container}>
@@ -36,18 +28,10 @@ const Header = ({ sidebarToggleHandler }) => {
       </div>
 
       <div className={styles.btn_group}>
-        <IconButton
-          id="mail"
-          onClick={handleClick}
-          badgeColor="var(--blue)"
-          badgeText={5}
-          badge={true}
-          icon={<MailIcon />}
-        ></IconButton>
+        <IconButton id="mail" badgeColor="var(--blue)" badgeText={5} badge={true} icon={<MailIcon />}></IconButton>
 
         <IconButton
           id="notifications"
-          onClick={handleClick}
           badgeColor="var(--red)"
           badgeText={11}
           badge={true}
@@ -56,7 +40,6 @@ const Header = ({ sidebarToggleHandler }) => {
 
         <div className={styles.btn_username}>{t.common.hello},&nbsp;Username</div>
       </div>
-      <HeaderMenu anchorEl={anchorEl} menuName={anchorEl?.id} handleClose={handleClose} />
     </header>
   )
 }
--- components/Layouts/Dashboard/Sidebar/SidebarFooter.tsx
+++ components/Layouts/Dashboard/Sidebar/SidebarFooter.tsx
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, { useState } from 'react'
 import styles from './SidebarFooter.module.css'
 import LanguageIcon from '../../../Icons/LanguageIcon'
 
