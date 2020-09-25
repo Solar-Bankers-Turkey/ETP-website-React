8054319879d55e70f64aa124f482c7f497973cd7
alicanerdurmaz <alicanerdurmaz@gmail.com>
2020-09-25 13:00:26 +0300

    when hover on header links, show border

--- components/Layouts/Dashboard/Header/HeaderInfo.tsx
+++ components/Layouts/Dashboard/Header/HeaderInfo.tsx
@@ -18,22 +18,25 @@ const HeaderInfo = () => {
         <span className={styles.coin_name}>IYTECOIN</span>
         <button className={styles.btn_reload}>{t.common.reload}</button>
       </div>
+
+      <div className={styles.energy_container}>
+        <div className={styles.solar_icon}>
+          <SolarPanelIcon />
+        </div>
+        <div className={styles.solar_value}>2.6</div>
+        <div className={styles.solar_text}>KWH</div>
+      </div>
       <Link href="/dashboard/battery">
-        <div className={styles.energy_container}>
-          <div className={styles.solar_icon}>
-            <SolarPanelIcon />
+        <div className={styles.battery_container}>
+          <div className={styles.battery_icon}>
+            <BatteryIcon />
           </div>
-          <div className={styles.solar_value}>2.6</div>
-          <div className={styles.solar_text}>KWH</div>
+          <span className={styles.battery_number}>50%</span>
+          <div className={styles.battery_indicator}>
+            <div className={styles.battery_fill}></div>
+          </div>
         </div>
       </Link>
-      <div className={styles.battery_icon}>
-        <BatteryIcon />
-      </div>
-      <span className={styles.battery_number}>50%</span>
-      <div className={styles.battery_indicator}>
-        <div className={styles.battery_fill}></div>
-      </div>
     </div>
   )
 }
